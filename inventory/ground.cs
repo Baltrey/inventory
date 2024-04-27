c8e544162647b2faffe827ee63840d624753a33a
Baltazar Biverstal <baltazar.biverstal@elev.ga.ntig.se>
2024-04-27 21:39:18 +0200

    Update

--- inventory/ground.cs
+++ inventory/ground.cs
@@ -28,44 +28,27 @@ public class Ground
     public void Groundstart()
     {
         items.Clear();
-        string[] NamesItem = { "Pouch", "Arrows", "Copper Ingot", "Iron Ingot", "Gold Ingot" };
-        string[] NamesWeapon = { "Axe", "Sword", "Bow", "Dagger", "Pickaxe" };
-
 
         for (int i = 0; i < 6; i++)
         {
-            Item item = new();
-            int number = Random.Shared.Next(NamesItem.Length);
-            if (number > 1)
-            {
-                WeightedItems weightedItems = new();
-                if (i % 2 == 0)
-                {
-                    weightedItems.Weight = "Light ";
-                }
-                else
-                {
-                    weightedItems.Weight = "Heavy ";
-                }
-                weightedItems.Name = NamesItem[number];
-                weightedItems.Space = Random.Shared.Next(1, 20);
-                items.Add(weightedItems);
-            }
-            else
-            {
-                item.Name = NamesItem[number];
-                item.Space = Random.Shared.Next(1, 20);
-                items.Add(item);
-            }
+            WeightedItems item = new();
+            int number = Random.Shared.Next(item.NamesWeightedItems.Length);
+            item.Name = item.getNames(number);
+            item.Weight = item.GetWeight(i);
+            //borde ändra
+            items.Add(item);
             //sätter ut heavy på alla items som kan vara heavy
         }
         //skapar alla items med weight och de som inte ska ha weight i ground
         for (int i = 0; i < 6; i++)
         {
             Weapon weapon = new();
-            int number = Random.Shared.Next(NamesWeapon.Length);
-            weapon.Name = NamesWeapon[number];
-            weapon.Space = Random.Shared.Next(1, 20);
+            int number = Random.Shared.Next(weapon.NamesWeapon.Length);
+            weapon.Name = weapon.getNames(number);
+            //hämtar namn
+            weapon.Length = weapon.Getlength(i);
+            weapon.Weight = weapon.GetWeight(i);
+            //sätter heavy och long på varanan vapen, dervärsa med ligth
             //skapar ett vapen från klassen och sätter ut namn och space med hjälp av random generator och tidigare array
             if (weapon.Space >= 18)
             {
@@ -75,22 +58,8 @@ public class Ground
                 // shinyWeapon.Space = weapon.Space;
                 // shinyWeapon.shiny = true;
             }
-
-            weapon.Weight = i % 2 == 0 ? "Light" : "Heavy";
-
-            if (i % 2 == 0)
-            {
-                weapon.Weight = "Light ";
-                weapon.Length = "Short ";
-            }
-            else
-            {
-                weapon.Weight = "Heavy ";
-                weapon.Length = "Long ";
-            }
+            //borde ändra senare
             items.Add(weapon);
-            //sätter heavy och long på varanan vapen, dervärsa med ligth
-
         }
         //skapar weapon items i ground
     }
--- inventory/item.cs
+++ inventory/item.cs
@@ -5,7 +5,7 @@ public class Item
     public string? Name;
     //items namn
 
-    int _space;
+    int _space = Random.Shared.Next(1, 20);
     public int Space
     {
         get
@@ -22,21 +22,84 @@ public class Item
         }
     }
     //sätter gränserna att ett items space value bara kan vara mellan 0 och 20
+
 }
 //skapar klassen items
 public class WeightedItems : Item
 {
     public string? Weight;
+    string[] _NamesWeightedItems = { "Pouch", "Arrows", "Copper Ingot", "Iron Ingot", "Gold Ingot" };
+    public string[] NamesWeightedItems
+    {
+        get
+        {
+            return _NamesWeightedItems;
+        }
+    }
+
+    public virtual string getNames(int item)
+    {
+        string i = _NamesWeightedItems[item];
+
+        return i;
+    }
+    //metod för att få ut namn på item
+    public string GetWeight(int times)
+    {
+        string i;
+        if (times % 2 == 0)
+        {
+            i = "Light ";
+        }
+        else
+        {
+            i = "Heavy ";
+        }
+        return i;
+
+    }
+    //metod för att få ut Weight på items
 }
 //skapar subclass weighted items av item
 public class Weapon : WeightedItems
 {
     public string? Length;
+    string[] _NamesWeapon = { "Axe", "Sword", "Bow", "Dagger", "Pickaxe" };
+    public string[] NamesWeapon
+    {
+        get
+        {
+            return _NamesWeapon;
+        }
+    }
+    public override string getNames(int item)
+    {
+        string i = _NamesWeapon[item];
+
+        return i;
+    }
+    //ändrar metoden getNames så den följer listan namesWeapon istället
+    public string Getlength(int times)
+    {
+        string i;
+        if (times % 2 == 0)
+        {
+            i = "Short";
+        }
+        else
+        {
+            i = "Long";
+        }
+        return i;
+    }
+    //metod för att få längd på Weapon
 
 }
+//subclass weapon av weightedItems
 public class Shiny : Weapon
 {
     public bool shiny;
+
 
 }
 //Skapar en subclass Weapon av Weighteditems
